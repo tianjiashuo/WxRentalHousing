ae0e50df0a51362dbe2d76871c456392b38516ab
Your Name <you@example.com>
2020-09-08 11:31:19 +0800

    newsAdd

---
+++ rental/src/main/java/com/rental/demo/Repository/entity/News.java
@@ -0,0 +1,34 @@
+package com.rental.demo.Repository.entity;
+
+public class News {
+
+    private int id;
+    private int user_id;
+    private String content;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+
+}
---
+++ rental/src/main/java/com/rental/demo/Repository/mappers/NewsRowMapper.java
@@ -0,0 +1,20 @@
+package com.rental.demo.Repository.mappers;
+
+import com.rental.demo.Repository.entity.Image;
+import com.rental.demo.Repository.entity.News;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class NewsRowMapper  implements RowMapper<News> {
+
+    @Override
+    public News mapRow(ResultSet rs, int rowNum) throws SQLException {
+        News news = new News();
+        news.setId(rs.getInt("id"));
+        news.setUser_id(rs.getInt("user_id"));
+        news.setContent(rs.getString("content"));
+        return news;
+    }
+}
