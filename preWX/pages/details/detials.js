3cf2bc1973d16d08981cc485289d56eb4bd6d9a8
liqingying <>
2020-09-14 23:08:39 +0800

    添加了部分详情页面的数据

--- preWX/pages/details/detials.js
+++ preWX/pages/details/detials.js
@@ -1,14 +1,17 @@
 // pages/details/detials.js
 Page({
+ 
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    address:""
+  },
   houseInfor(e) {
     wx.navigateTo({
       url: '../exploration/exploration',
     })
   },
-  /**
-   * 页面的初始数据
-   */
-  data: {},
   Tarvel(e) {
     let sTop = this.data.scrollTop;
     console.log(sTop);
@@ -71,13 +74,48 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    id: 1,
+    hostId:0,
+    shortestLease:0,
+    area: 0,
+    price: 0,
+    floor: 0,
+    address: "",
+    title: "",
+    type: "",
+    orientation: "",
+    furniture: "",
+    isElevator: false,
+    isPet: false,
+    isForm: false,
+    isState: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+
+    let that = this;
+    wx.request({
+      url: 'http://47.94.170.167:8080/rentAllInfo/1',
+      method:'GET',
+      header: {
+        'Content-Type': 'application/json'
+      },
+      success: function (res) {
+        console.log(res.data)
+        that.setData({
+         title:res.data.rentInfo.title,
+         address:res.data.rentInfo.address,
+         type:res.data.rentInfo.type,
+         orientation:res.data.rentInfo.orientation,
+         furniture:res.data.rentInfo.furniture,
+         isElevator:res.data.rentInfo.isElevator,
+         shortestLease:res.data.rentInfo.shortestLease
+        })
+      }
+    })
 
   },
 
